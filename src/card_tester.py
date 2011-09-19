f42b6b1f167ed7d84fd8e764fd473ff894654869
unknown <Entrail@.(none)>
2011-09-19 21:42:32 +0200

    fikset cards

--- src/card_tester.py
+++ src/card_tester.py
@@ -1,5 +1,23 @@
 import cards
 
 kort = cards.gen_52_shuffled_cards()
-for k in kort:
-    print k
+#for k in kort:
+#    print k
+
+deck1 = []
+print "Deck 1: "
+for i in range(5):
+    deck1.append(kort.pop())
+    print deck1[i]
+print "Deck 1 power: "
+print cards.calc_cards_power(deck1)
+
+deck2 = []
+print "Deck 2: "
+for i in range(5):
+    deck2.append(kort.pop())
+    print deck2[i]
+print "Deck 2 power: "
+print cards.calc_cards_power(deck2)
+
+
