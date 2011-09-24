054dff0d87c4654b7c059b12e69d984412195472
Ole-Petter <Entrail@.(none)>
2011-09-24 17:11:54 +0200

    hand strength

---
+++ src/hand_strength.py
@@ -0,0 +1,70 @@
+import cards
+import itertools
+
+def calculateHandStrength(hand, numberOfOpponents, tableCards numberOfSimulations):
+    playerHand = hand[:]
+    table = tableCards[:]
+    numberOfWins = 0
+    numberOfTies = 0
+    numberOfLosses = 0
+    for simulation in numberOfSimulations:
+        remainingCards = cards.gen_52_shuffled_cards()
+        for c in playerHand: 
+            remainingCards.remove(c)
+        for c in table: 
+            remainingCards.remove(c)
+        opponents = []
+        for foo in numberOfOpponents:
+            o = []
+            o.append(remainingCards.pop())
+            o.append(remainingCards.pop())
+            opponents.append(o)
+        for i in range(5 - len(tableCards)):
+            table.append(remainingCards.pop())
+        outcome = calculateOutcome(playerHand, table, opponents)
+        if outcome == 1:
+            numberOfWins += 1
+        elif outcome == 0:
+            numberOfTies += 1
+        else:
+            numberOfLosses += 1
+    base_strength = float(float(float(numberOfWins) + float(float(numberOfTies)/float(2))) / float(numberOfSimulations))
+    hand_strength = float(1)
+    for i in range(numberOfOpponents):
+        hand_strength = hand_strength * base_strength
+    return hand_strength
+    
+        
+        
+
+
+def calculateOutcome(player, tableCards, opponents):
+    winners = []
+    currentWinner = player
+    hand = player[:]
+    for c in tableCards:
+        hand.append(c)
+    winningHand = cards.calc_cards_power(hand)
+    winners.append(hand)
+    for o in opponents:
+        ny_hand = o[:]
+        for c in tableCards:
+            ny_hand.append(c)
+        power = cards.calc_cards_power(ny_hand)
+        for i in range(len(power)):
+            if power[i] > winningHand[i]:
+                currentWinner = ny_hand
+                winningHand = power
+                winners = [currentWinner]
+                break
+            elif power[i] < winningHand[i]:
+                break
+            elif i == len(power):
+                winners.append(ny_hand)
+    if not hand in winners:
+        return -1
+    elif len(winners) == 1:
+        return 1
+    else:
+        return 0
+    
