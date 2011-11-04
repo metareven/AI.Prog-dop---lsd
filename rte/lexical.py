30c3ecb7d22119b360e0a4030dd1a5ecd5c765f3
Ole-Petter <ole-petter@ubuntu.(none)>
2011-11-04 14:00:55 +0100

    lese av alle t/h par fra xml-filen

--- rte/lexical.py
+++ rte/lexical.py
@@ -3,18 +3,33 @@ import xml.dom.minidom
 def word_matching():
     return 0
 
-# function for reading an xml document from file
-# takes the url of an xml document as input and returns a dom instance of the xml
+# Function for reading an xml document from file
+# Takes the url of an xml document as input and returns a dom instance of the xml
 def get_a_document_from_file(file_object):
     return xml.dom.minidom.parse(file_object)
 
-# main method for testing purposes
+# Takes a list of elements nodes with name "pair" and returns a list of string pairs (text, hypotesis)
+def get_text_pairs_from_pair_nodes(pair_nodes):
+    pairs = []
+    for pair in pair_nodes:
+        text = pair.getElementsByTagName("t")[0].childNodes[0].nodeValue
+        hypothesis = pair.getElementsByTagName("h")[0].childNodes[0].nodeValue
+        pairs.append((text,hypothesis))
+    return pairs
+
+def get_pair_nodes(doc):
+    pairs = doc.getElementsByTagName("pair")
+    return pairs
+
+
+# Main method for testing purposes
 def main():
     test_document = get_a_document_from_file("data/RTE2_dev.xml")
-    print test_document.childNodes
-    # test_document -> root node -> proper child -> 1 for t / 3 for h
-    print test_document.childNodes[1].childNodes[1].childNodes[1].childNodes[0].nodeValue # only the odd numbered children are actual pairs
-    print test_document.childNodes[1].nodeType
+    pair_nodes = get_pair_nodes(test_document)
+    pairs = get_text_pairs_from_pair_nodes(pair_nodes)
+    print len(pairs)
+    print pairs[0]
+    print pairs[len(pairs)-1]
 
 if __name__ == '__main__':
     main()
