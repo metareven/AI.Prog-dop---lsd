df52a7d80f40369af8b7fac975d53df9eb95bdb7
unknown <Ole-Petter@hakurei.(none)>
2011-11-23 18:45:28 +0100

    added the phase 1 and 2 methods

--- rte/rte.py
+++ rte/rte.py
@@ -9,7 +9,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
-import bleu,pos,machine_learning,idf_weighting,syntax_matching
+import bleu,pos,machine_learning,idf_weighting,syntax_matching,lemma,lexical,tree_edit_dist
 def main():
     text = ""
     while text.lower() != "exit":
@@ -26,6 +26,16 @@ def main():
             machine_learning.main(4,False)
         elif text == "four make":
             machine_learning.main(4,True)
+        elif text == "lemma":
+            lemma.main()
+        elif text == "bleu":
+            bleu.main()
+        elif text == "lexical":
+            lexical.main()
+        elif text == "tree distance":
+            tree_edit_dist.main()
+        elif text == "idf weighting":
+            idf_weighting.main()
         elif text != "exit":
             print "invalid input: input must be one of the following items:"
             print "one\ntwo\nthree\nfour"
